953571c5ccd4e073bbbb4c1c84275c3221e2a26a
Jake Kressley <kressleyjake@gmail.com>
2023-01-06 00:31:17 -0500

    working navbar and sidebar

--- website-smooth-please/src/App.js
+++ website-smooth-please/src/App.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import React from 'react'
 import './App.css';
+import Navbar from './components/Navbar';
 import {BrowserRouter as Router} from 'react-router-dom';
-import Home from './pages';
-
-
 
 function App() {
   return (
-    <Router>
-      <Home />
-    </Router>
+    <div>
+      <Navbar />
+    </div>
   );
-};
+}
 
 export default App;
---
+++ website-smooth-please/src/components/Navbar/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo
+} from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo>
+                    Jake
+                </NavLogo>
+            </NavbarContainer>
+        </Nav>  
+    </>
+  );
+};
+
+export default Navbar
