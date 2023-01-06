7c8a9a65ab146485aed8541bfbcd5087d25c309e
Jake Kressley <kressleyjake@gmail.com>
2023-01-06 00:54:04 -0500

    displays video halfway through hero section

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Video from '../../videos/video.mp4'
+import {
+    HeroContainer,
+    HeroBg,
+    VideoBg
+} from './HeroElements';
+
+const HeroSection = () => {
+  return (
+    <HeroContainer id="home">
+        <HeroBg>
+            <VideoBg autoPlay loop muted src={Video} type='/video/mp4' />
+        </HeroBg>
+    </HeroContainer>
+  )
+}
+
+export default HeroSection
